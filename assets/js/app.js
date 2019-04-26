c2eb09363f60327b7bade2e927b1f7f155098dc8
unknown <nikoladimitrijevic1991@gmail.com>
2019-04-26 13:50:21 +0200

    button effect

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,32 @@ function vidDivFun() {
   vidDiv.style.right = '0';
 }
 
+// button hover
+// button hover
+// button hover
+var getStartedNowButton = document.getElementsByClassName('getStartedNowButton')[0];
+var buttonHoverDiv = document.getElementsByClassName('buttonHoverDiv')[0];
+
+
+getStartedNowButton.addEventListener('mouseenter',()=>{
+  buttonHoverDiv.style.height = '51%';
+  setTimeout(function () {
+    buttonHoverDiv.style.top = "50%"
+  }, 150);
+});
+
+getStartedNowButton.addEventListener('mouseleave',()=>{
+  buttonHoverDiv.style.top = "0%"
+  // buttonHoverDiv.style.height = '0%';
+  setTimeout(function () {
+    buttonHoverDiv.style.height = '0%';
+  }, 150);
+});
+
+
+
+
+
 window.onload = function(){
   vidDivFun();
 }
